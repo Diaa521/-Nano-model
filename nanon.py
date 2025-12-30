93aef87d82add129753158cef8dfc25b42f2ba0a
Amazing Code (ac) <Shikhsulaimandiaa@gmail.com>
2025-12-30 09:39:41 +0100

    Create nanon.py

---
+++ nanon.py
@@ -0,0 +1,12 @@
+# Simplest case: y = m*x (without B or others)
+x = 3
+y = 6
+
+# Direct billing without training
+m = y / x
+
+print("Willkomme to the mini Modell!")
+
+# Reschnung
+print(f"Model: y = {m} * x")
+print(f"Test: Für x=4  => y = {m * 4} ")
